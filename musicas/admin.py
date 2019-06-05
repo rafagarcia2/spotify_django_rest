03607bdba3bc4befd1ba90ec8acc6c365b7a5faa
Rafael Garcia <rafaelgarcia2013@gmail.com>
2019-06-05 09:55:30 -0300

    Adicionando Modelos ao Admin e fixture com dados de músicas

--- musicas/admin.py
+++ musicas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Musica, Playlist
 
-# Register your models here.
+class MusicaAdmin(admin.ModelAdmin):
+	list_display = ['pk', 'titulo', 'cantor', 'ano_lancamento']
+	search_fields = ['titulo', 'cantor']
+	list_filter = ['titulo']
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+	search_fields = ['titulo', 'usuario', 'data_criacao']
+	list_filter = ['titulo', 'usuario']
+
+
+admin.site.register(Musica, MusicaAdmin)
+admin.site.register(Playlist, PlaylistAdmin)
